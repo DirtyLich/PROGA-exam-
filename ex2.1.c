41dc31d1c8327030fa22b8e46cc784b548a7403a
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 20:40:24 +0700

    Update ex2.1.c

--- ex2.1.c
+++ ex2.1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
-#include <string.h>
+
+// Функция для вычисления длины строки
+int custom_strlen(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') { // Пока не достигнут символ конца строки
+        length++; // Увеличиваем счетчик длины
+    }
+    return length; // Возвращаем длину строки
+}
 
 int is_valid_prefix(char* str, char* valid_chars) {
-    int len = strlen(str);
-    int valid_len = strlen(valid_chars);
+    int len = custom_strlen(str); // Вычисляем длину строки str
+    int valid_len = custom_strlen(valid_chars); // Вычисляем длину строки valid_chars
     int i, j;
-    for (i = 0; i < len; i++) {
-        char c = str[i];
-        int found = 0;
-        for (j = 0; j < valid_len; j++) {
-            if (c == valid_chars[j]) {
-                found = 1;
-                break;
+    for (i = 0; i < len; i++) { // Проходим по каждому символу в строке str
+        char c = str[i]; // Получаем текущий символ
+        int found = 0; // Флаг для указания, найден ли символ в списке допустимых символов
+        for (j = 0; j < valid_len; j++) { // Проходим по каждому символу в списке допустимых символов
+            if (c == valid_chars[j]) { // Если текущий символ совпадает с символом из списка
+                found = 1; // Устанавливаем флаг found в значение 1
+                break; // Прерываем цикл, так как символ уже найден
             }
         }
-        if (!found) {
-            return i;
+        if (!found) { // Если символ не найден в списке допустимых символов
+            return i; // Возвращаем позицию символа, на котором нарушается условие
         }
     }
-    return len;
+    return len; // Если все символы в строке являются допустимыми, возвращаем длину строки
 }
 
 int main() {
-    char* str = "abc123xyz";
-    char* valid_chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    int prefix_len = is_valid_prefix(str, valid_chars);
-    printf("Valid prefix length: %d\n", prefix_len);
+    char* str = "abc123xyz"; // Исходная строка
+    char* valid_chars = "abcdefghijklmnopqrstuvwxyz0123456789"; // Список допустимых символов
+    int prefix_len = is_valid_prefix(str, valid_chars); // Вызов функции для проверки префикса
+    printf("Valid prefix length: %d\n", prefix_len); // Вывод длины допустимого префикса
     return 0;
-} 
+}
