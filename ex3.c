2802b2b8d35808a5a24564f84d9377048065a37e
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 20:43:51 +0700

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
+// Функция для получения допустимого префикса
 char* get_valid_prefix(char* str) {
-    char* prefix = str;
-    int len = strlen(str);
-    for (int i = 0; i < len; i++) {
-        if (!isalpha(str[i]) && !isdigit(str[i])) {
-            prefix[i] = '\0';
-            break;
+    char* prefix = str; // Указатель на начало строки является префиксом по умолчанию
+    int len = strlen(str); // Вычисляем длину строки
+    for (int i = 0; i < len; i++) { // Проходим по каждому символу в строке
+        if (!isalpha(str[i]) && !isdigit(str[i])) { // Проверяем, является ли символ буквой или цифрой
+            prefix[i] = '\0'; // Если символ не является допустимым, устанавливаем его на место завершающего нуля
+            break; // Прекращаем цикл, так как достигнут недопустимый символ
         }
     }
-    return prefix;
+    return prefix; // Возвращаем префикс
 }
 
 int main() {
-    char str[] = "abc123!@#";
-    char* prefix = get_valid_prefix(str);
-    printf("Prefix: %s\n", prefix);
+    char str[] = "abc123!@#"; // Исходная строка
+    char* prefix = get_valid_prefix(str); // Вызов функции для получения допустимого префикса
+    printf("Prefix: %s\n", prefix); // Вывод допустимого префикса
     return 0;
 }
