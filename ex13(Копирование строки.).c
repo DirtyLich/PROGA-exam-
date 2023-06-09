00627cf7626f9470d2ff2331d5ee8311b5fc3f0b
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 21:31:15 +0700

    Update ex13(Копирование строки.).c

--- ex13(Копирование строки.).c
+++ ex13(Копирование строки.).c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+// Функция для копирования строки
+void custom_strcpy(char* destination, const char* source) {
+    int i = 0;
+    while (source[i] != '\0') {
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i] = '\0'; // Добавляем завершающий нуль в конец скопированной строки
+}
+
+int main() {
+    char source[] = "Hello, world!";
+    char destination[100];
+
+    custom_strcpy(destination, source);
+
+    printf("Source: %s\n", source);
+    printf("Destination: %s\n", destination);
+
+    return 0;
+}
 
