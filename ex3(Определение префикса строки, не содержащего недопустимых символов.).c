e9cd91bb4382bf5207d944ef8ccf95f72b60a641
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 20:48:01 +0700

    Update ex3(Определение префикса строки, не содержащего недопустимых символов.).c

--- ex3(Определение префикса строки, не содержащего недопустимых символов.).c
+++ ex3(Определение префикса строки, не содержащего недопустимых символов.).c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+// Функция для вычисления длины строки
+int custom_strlen(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
 // Функция для получения допустимого префикса
 char* get_valid_prefix(char* str) {
     char* prefix = str; // Указатель на начало строки является префиксом по умолчанию
-    int len = strlen(str); // Вычисляем длину строки
+    int len = custom_strlen(str); // Вычисляем длину строки с помощью пользовательской функции
     for (int i = 0; i < len; i++) { // Проходим по каждому символу в строке
         if (!isalpha(str[i]) && !isdigit(str[i])) { // Проверяем, является ли символ буквой или цифрой
             prefix[i] = '\0'; // Если символ не является допустимым, устанавливаем его на место завершающего нуля
