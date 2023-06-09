45040138bbb08c19296b2b9744e429dca4f37b22
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 20:21:55 +0700

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,46 +1,48 @@
 #include <stdio.h>
 
+// Функция для вычисления длины строки
 int custom_strlen(const char* str) {
     int length = 0;
-    while (str[length] != '\0') {
+    while (str[length] != '\0') {  // Итеративно увеличиваем длину до достижения нулевого символа
         length++;
     }
     return length;
 }
 
+// Функция для проверки корректности IP-адреса
 int is_valid_ip_address(const char* ip_address) {
     int num, dots = 0;
     char c;
-    int len = custom_strlen(ip_address);
+    int len = custom_strlen(ip_address);  // Получаем длину строки IP-адреса
     int i = 0;
     
     for(i = 0; i < len; i++) {
         c = ip_address[i];
         if(c == '.') {
             dots++;
-            if(dots > 3) {
-                return 0;
+            if(dots > 3) {  // Проверяем, что количество точек не превышает 3
+                return 0;  // Возвращаем 0, если IP-адрес некорректный
             }
             continue;
         }
-        if(c < '0' || c > '9') {
-            return 0;
+        if(c < '0' || c > '9') {  // Проверяем, что символ является цифрой
+            return 0;  // Возвращаем 0, если IP-адрес некорректный
         }
         num = 0;
-        while(c >= '0' && c <= '9') {
+        while(c >= '0' && c <= '9') {  // Извлекаем число из цифровой группы
             num = num * 10 + (c - '0');
-            if(num > 255) {
-                return 0;
+            if(num > 255) {  // Проверяем, что число не превышает 255
+                return 0;  // Возвращаем 0, если IP-адрес некорректный
             }
             i++;
             c = ip_address[i];
         }
         i--;
     }
-    if(dots != 3) {
-        return 0;
+    if(dots != 3) {  // Проверяем, что количество точек равно 3
+        return 0;  // Возвращаем 0, если IP-адрес некорректный
     }
-    return 1;
+    return 1;  // Возвращаем 1, если IP-адрес корректный
 }
 
 int main() {
