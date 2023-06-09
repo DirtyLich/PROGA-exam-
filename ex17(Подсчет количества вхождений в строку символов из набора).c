c077e76b3167a1d1b00d368cf33280d09426eae8
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 21:46:10 +0700

    Update ex17(Подсчет количества вхождений в строку символов из набора).c

--- ex17(Подсчет количества вхождений в строку символов из набора).c
+++ ex17(Подсчет количества вхождений в строку символов из набора).c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+// Функция для подсчета количества вхождений символов из набора в строку
+int count_occurrences(const char* str, const char* chars) {
+    int count = 0;
+    while (*str != '\0') {
+        const char* ch = chars;
+        while (*ch != '\0') {
+            if (*str == *ch) {
+                count++;
+                break;
+            }
+            ch++;
+        }
+        str++;
+    }
+    return count;
+}
+
+int main() {
+    const char* str = "Hello, World!";
+    const char* chars = "lo"; // Набор символов для поиска
+    int occurrences = count_occurrences(str, chars);
+
+    printf("Number of occurrences: %d\n", occurrences);
+
+    return 0;
+}
 
