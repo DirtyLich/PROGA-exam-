91368b23735d79bae79385abfb264eb9d3f7ea69
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 21:39:27 +0700

    Update ex15(Преобразование snake_case в CamelCase).c

--- ex15(Преобразование snake_case в CamelCase).c
+++ ex15(Преобразование snake_case в CamelCase).c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+// Функция для преобразования символа в заглавную букву
+char custom_toupper(char c) {
+    if (c >= 'a' && c <= 'z') {
+        return c - 'a' + 'A';
+    }
+    return c;
+}
+
+// Функция для преобразования snake_case в CamelCase
+void convert_snake_to_camel(char* str) {
+    int i = 0;
+    int j = 0;
+    int capitalize = 0;
+
+    while (str[i] != '\0') {
+        if (str[i] == '_') {
+            capitalize = 1; // Установка флага для следующего символа
+        } else {
+            if (capitalize) {
+                str[j] = custom_toupper(str[i]); // Преобразование символа в заглавную букву
+                capitalize = 0; // Сброс флага
+            } else {
+                str[j] = str[i];
+            }
+            j++;
+        }
+        i++;
+    }
+    str[j] = '\0'; // Добавляем завершающий нуль
+}
+
+int main() {
+    char str[] = "snake_case_string";
+    printf("Before conversion: %s\n", str);
+
+    convert_snake_to_camel(str);
+
+    printf("After conversion: %s\n", str);
+
+    return 0;
+}
 
