6d5bf4687d30882661804c94365a0f43740d65fe
DirtyLich <124029168+DirtyLich@users.noreply.github.com>
2023-06-09 20:06:40 +0700

    Update 1exe.c

--- 1exe.c
+++ 1exe.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Функция для проверки, является ли символ цифрой
 bool isDigit(char c) {
     return c >= '0' && c <= '9';
 }
 
+// Функция для проверки корректности IP-адреса
 bool isValidIpAddress(const char *ipAddress) {
     int num;
-    int dots = 0;
-    int segments = 0;
-    int i = 0;
-    int segmentStart = 0;
+    int dots = 0;            // Количество точек в IP-адресе
+    int segments = 0;        // Количество сегментов в IP-адресе
+    int i = 0;               // Индекс текущего символа
+    int segmentStart = 0;    // Индекс начала текущего сегмента
     
+    // Проверяем каждый символ IP-адреса
     while (ipAddress[i] != '\0') {
         if (ipAddress[i] == '.') {
             dots++;
@@ -21,6 +24,8 @@ bool isValidIpAddress(const char *ipAddress) {
             if (i - segmentStart == 0) {
                 return false;
             }
+            
+            // Парсим числовое значение сегмента
             num = 0;
             for (int j = segmentStart; j < i; j++) {
                 if (!isDigit(ipAddress[j])) {
@@ -28,9 +33,12 @@ bool isValidIpAddress(const char *ipAddress) {
                 }
                 num = num * 10 + (ipAddress[j] - '0');
             }
+            
+            // Проверяем диапазон числового значения
             if (num < 0 || num > 255) {
                 return false;
             }
+            
             segments++;
             segmentStart = i + 1;
         } else if (!isDigit(ipAddress[i])) {
@@ -39,6 +47,7 @@ bool isValidIpAddress(const char *ipAddress) {
         i++;
     }
     
+    // Проверяем количество точек и сегментов в IP-адресе
     if (segments != 3 || dots != 3) {
         return false;
     }
